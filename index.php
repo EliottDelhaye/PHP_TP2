1aa6f2b0a4e3ca1e3bab6e1dd5e1aeb59da871bc
EliottDELHAYE <91311354+EliottDELHAYE@users.noreply.github.com>
2021-10-20 16:46:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+define('SQLUSR', 'root');
+define('SQLPWD', 'root');
+require_once('./db.php');
+
+if(!empty($_REQUEST['addStock'])) {
+    // ajout stock
+}
+
+if(!empty($_REQUEST['addSale'])) {
+    // ajout vente
+}
+
+?><!doctype html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf8" />
+    </head>
+    <body>
+        
+    </body>
+</html>
