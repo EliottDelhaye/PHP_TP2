368af9cdb92811425083e4fc0623afe84f05d4e1
Eliott DELHAYE <e.delhaye@P210.sn>
2021-11-24 12:01:18 +0100

    v2

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
 define('SQLUSR', 'root');
-define('SQLPWD', 'root');
+define('SQLPWD', '');
 require_once('./db.php');
 
 if(!empty($_REQUEST['addStock'])) {
-    // ajout stock 111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    // ajout stock 
 }
 
 if(!empty($_REQUEST['addSale'])) {
     // ajout vente
 }
 
-?><!doctype html>
+?>
+
+<!doctype html>
 <html>
     <head>
-        <title></title>
         <meta charset="utf8" />
+        <title>[ADMIN] Autos Management</title>
+        <h1>Gestionnaire</h1>
+        <p>Ce gestionnaire est un test réalisé par E.DELHAYE</p>
+       
+
+        // arrive partie3 du TP 
+
+
+        <footer>Copyright <a title="DELHAYE Eliott">E.DELHAYE</a> </footer>
+
+        
+
+
     </head>
     <body>
         
